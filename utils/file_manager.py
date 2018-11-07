43a94e9720568ef7b5b1983080d80fbab83f2045
zhangsihong123 <azsh19941014>
2018-11-07 17:21:29 +0800

    爬取y80s网站数据

---
+++ utils/file_manager.py
@@ -0,0 +1,45 @@
+#-*- coding:utf-8 -*-
+
+#写入数据的工具类
+class FileManager(object):
+
+    #写一个写入数据的接口
+    @classmethod
+    def write(self, filename, content, split = '\n'):
+        '''
+        :param content:要写入的数据
+        :param split: 每条数据之间的分隔符
+        :return:
+        '''
+        #判断传入的参数是否为字符串类型，如果是，写入数据，如果不是则抛出异常
+        if isinstance(content, str):
+            #打开文件
+            f = open(filename, 'a')
+            #写入数据
+            f.write(content)
+            f.write(split)
+            #关闭文件
+            f.close()
+        else:
+            raise TypeError('content must be a str!')
+    #写入多行数据
+    @classmethod
+    def writelines(self, filename, str_list, split= '\n'):
+        rs = isinstance(str_list, list)
+        #判断某个对象是否是某个类型，如果是返回True，如果不是返回False
+        if rs:
+            #遍历列表，取出每一数据，判断数据类型是否为字符串
+            for content in str_list:
+                #如果不是字符串类型
+                if isinstance(content, str) == False:
+                    #抛出异常
+                    raise TypeError('str_list must be a list of str!,such as:[str1,str2,...]')
+            #如果没抛出异常，就可以写入数据
+            f = open(filename, 'a')
+            string =split.join(str_list)
+            f.write(string)
+            f.close()
+        else:
+            #如果不是列表，抛出异常
+            raise TypeError('str_list must be a list of str,ex[str1, str2,...]')
+
--- y80sdemo/y80s_movie_spider.py
+++ y80sdemo/y80s_movie_spider.py
@@ -1,22 +1,27 @@
 # -*- coding:utf-8 -*-
 
 from bs4 import BeautifulSoup
-from urllib.request import urlretrieve
+#from urllib.request import urlretrieve
 import requests,os,time
+from utils.file_manager import FileManager
+
 
 if __name__=='__main__':
     server = 'https://www.80s.tw/'
     url = 'https://www.80s.tw/movie/list'
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+        'Referer':'https://www.80s.tw/movie/list',
+        'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Mobile Safari/537.36'
     }
     r = requests.get(url=url,headers=headers)
     bf = BeautifulSoup(r.text,'lxml')
-    clearfix = bf.find('ul',class_='me1 clearfix')
+    clearfix = bf.find(class_='me1 clearfix')
     clearfix_bf = BeautifulSoup(str(clearfix),'lxml')
     div_a = clearfix_bf.find_all('a')
 
-    #print(div_a)
+    #print(clearfix_bf)
+
+    fm = FileManager()
 
     for a in div_a:
         a_bf = BeautifulSoup(str(a),'lxml')
@@ -24,16 +29,53 @@ if __name__=='__main__':
         url_info = server+href
         print('详情链接',server + href) #迅雷谜中电影详情链接
         info = requests.get(url=url_info,headers=headers)
-        print(info.text)
-        info_bf = BeautifulSoup(info.text,'lxml')
-        clearfix_block1 = info_bf.find_all('div',class_='clearfix block1')
-        print(clearfix_block1)
 
+        info_bf = BeautifulSoup(info.text,'lxml')
+        minfo = info_bf.find('div',class_='clearfix',id='minfo')
+        img_info = info_bf.find('div', class_='img')
+
+        name = ''
+        src_url = ''
+        if img_info is not None:
+            name = img_info.img.get('title')
+            src_url = 'http:'+ img_info.img.get('src')
+        print(src_url)
+
+        desc = info_bf.find('div',id='movie_content',class_='clearfix')
+        desc_text = ''
+        try:
+            desc_text = desc.text
+        except AttributeError as e:
+            desc_text = '暂无介绍'
+        print(desc_text)
+
+        video_url = info_bf.find('span',class_='dlname nm')
+        try:
+            video_thunder = '迅雷链接：'+video_url.span.a.get('href')
+        except AttributeError as e:
+            video_thunder = '暂无链接'
+        print(video_thunder, '\n')
+        filename = 'y80s电影大全.txt'
+        fm.writelines(filename,str_list=[name,url_info,desc_text,video_thunder+'\n'])
+        # fm.write(filename,url_info,split='')
+        # fm.write(filename,desc_text,split='')
+        # fm.write(filename,video_thunder,split='\n\n')
+
+        time.sleep(1)
 
     print('下载完毕')
 
 
+
+
+
+
+
+
+
+
     #下载图片的#######################
+
     #p = clearfix_bf.find_all('img',class_='p')
 
     # for each in p:
